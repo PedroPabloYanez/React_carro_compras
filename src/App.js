ee49e0bf0cc5ca559d86aed55fd7b929252d0171
pedro <pedropablo.yanez@hotmail.com>
2021-10-11 18:12:46 -0300

    show elements en alert button

--- src/App.js
+++ src/App.js
@@ -10,16 +10,37 @@ class App extends Component {
       { name: 'Tomate', price: 1500, img: 'productos/tomate.jpg' },
       { name: 'Arbejas', price: 2500, img: 'productos/arbejas.jpg'},
       { name: 'Lechuga', price: 500, img: 'productos/lechuga.jpg'},
-    ]
+    ],
+    carro: []
   }
+
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state
+    if (carro.find(x => x.name == producto.name)){
+      const newCarro = carro.map(x => x.name === producto.name
+        ? ({
+          ...x,
+          cantidad: x.cantidad + 1
+        })
+        : x)
+      return this.setState({ carro: newCarro })
+    }
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      })
+    })
+  }
+
   render(){
     return(
       <div>
-        <Navbar />
+        <Navbar carro={this.state.carro}/>
         <Layout>
           <Title />
           <Productos 
-            agregarAlCarro={() => console.log('No hace nada')}
+            agregarAlCarro={this.agregarAlCarro}
             productos={this.state.productos}
           />
         </Layout>
